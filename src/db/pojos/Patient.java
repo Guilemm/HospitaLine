f424fafdde49a152a56338afb5283df10b23d444
Guilemm <guillermo.lozano.go@gmail.com>
2025-04-09 15:32:03 +0200

    lo nuevo
    Lo nuevp

--- src/db/pojos/Patient.java
+++ src/db/pojos/Patient.java
@@ -1,18 +1,30 @@
 package db.pojos;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Patient {
 	
 	public static void main(String args[])
 	{
-		System.out.println("bum");
-		System.out.println("Diana");
-		
-		//hola
-		//buenos dias
-		//JIJIJIJIJ
-		System.out.println("yyyy");
-		System.out.println("34");
-
+		try {
+			Class.forName("org.sqlite.JDBC");
+			Connection c= DriverManager.getConnection("jdbc:sqlite:Databasestep5.db");
+			//c.createStatement().execute();
+			Statement stmt=c.createStatement();
+			String query= "INSERT INTO medsupply(id, name, quantity) VALUES(35, 'hola caracola', 56)";
+			stmt.executeUpdate(query);
+			stmt.close();
+			System.out.println();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
