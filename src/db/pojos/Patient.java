ae73b079e8d215cb494e5ae92835595250a6ae4c
Guilemm <guillermo.lozano.go@gmail.com>
2025-04-09 16:03:55 +0200

    ya funciona

--- src/db/pojos/Patient.java
+++ src/db/pojos/Patient.java
@@ -14,17 +14,18 @@ public class Patient {
 			Connection c= DriverManager.getConnection("jdbc:sqlite:Databasestep5.db");
 			//c.createStatement().execute();
 			Statement stmt=c.createStatement();
-			String query= "INSERT INTO medsupply(id, name, quantity) VALUES(35, 'hola caracola', 56)";
+			String query= "INSERT INTO medsupply (id, name, quantity) VALUES(44, 'hol', 7.0)";
 			stmt.executeUpdate(query);
 			stmt.close();
 			System.out.println();
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			System.out.println("no tan guassa");
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
+			System.out.println("guasaaa");
 			e.printStackTrace();
 		}
 	}
-
 }
